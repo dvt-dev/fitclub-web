cef98f72d5565b6187d300952a025410b49be10e
Tuấn Đỗ Văn <dvtuan.dev@gmai.com>
2023-06-24 15:00:05 +0700

    responsive navbar menu

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -25,69 +25,81 @@ const Hero = () => {
                 {/* Left side */}
                 <div className={cx("hero__left")}>
                     <Header />
-                    {/* The best ad */}
-                    <div className={cx("the-best--ad")}>
-                        <motion.div
-                            initial={{ left: "208px" }}
-                            whileInView={{ left: "8px" }}
-                            transition={{ ...transition, type: "tween" }}
-                        ></motion.div>
-                        <span className={cx("the-best--ad-text")}>
-                            the best fitness club in the town
-                        </span>
-                    </div>
+                    <div className={cx("hero__left-content")}>
+                        {/* The best ad */}
+                        <div className={cx("the-best--ad")}>
+                            <motion.div
+                                initial={{ left: "208px" }}
+                                whileInView={{ left: "8px" }}
+                                transition={{ ...transition, type: "tween" }}
+                            ></motion.div>
+                            <span className={cx("the-best--ad-text")}>
+                                the best fitness club in the town
+                            </span>
+                        </div>
 
-                    {/* Hero heading */}
-                    <div className={cx("hero__heading")}>
-                        <div>
-                            <span className={cx("stroke-text")}>Shape </span>
-                            <span>your</span>
+                        {/* Hero heading */}
+                        <div className={cx("hero__heading")}>
+                            <div>
+                                <span className={cx("stroke-text")}>
+                                    Shape{" "}
+                                </span>
+                                <span>your</span>
+                            </div>
+                            <div>
+                                <span>Ideal </span>
+                                <span>body</span>
+                            </div>
+                            <p className={cx("hero__text")}>
+                                In here we will help you to shape and build your
+                                ideal body and live up your life to fullest
+                            </p>
                         </div>
-                        <div>
-                            <span>Ideal </span>
-                            <span>body</span>
-                        </div>
-                        <p className={cx("hero__text")}>
-                            In here we will help you to shape and build your
-                            ideal body and live up your life to fullest
-                        </p>
-                    </div>
 
-                    {/* Figures */}
-                    <div className={cx("figures")}>
-                        <div className={cx("figures__item")}>
-                            <span className={cx("figures__item-number")}>
-                                <span>+</span>
-                                <CountUp start={100} end={140} duration={4} />
-                            </span>
-                            <span className={cx("figures__item-des")}>
-                                EXPERT COACHES
-                            </span>
+                        {/* Figures */}
+                        <div className={cx("figures")}>
+                            <div className={cx("figures__item")}>
+                                <span className={cx("figures__item-number")}>
+                                    <span>+</span>
+                                    <CountUp
+                                        start={100}
+                                        end={140}
+                                        duration={4}
+                                    />
+                                </span>
+                                <span className={cx("figures__item-des")}>
+                                    EXPERT COACHES
+                                </span>
+                            </div>
+                            <div className={cx("figures__item")}>
+                                <span className={cx("figures__item-number")}>
+                                    <span>+</span>
+                                    <CountUp
+                                        start={888}
+                                        end={978}
+                                        duration={4}
+                                    />
+                                </span>
+                                <span className={cx("figures__item-des")}>
+                                    MEMBERS JOINED
+                                </span>
+                            </div>
+                            <div className={cx("figures__item")}>
+                                <span className={cx("figures__item-number")}>
+                                    <span>+</span>
+                                    <CountUp start={0} end={50} duration={4} />
+                                </span>
+                                <span className={cx("figures__item-des")}>
+                                    FITNESS PROGRAMS
+                                </span>
+                            </div>
                         </div>
-                        <div className={cx("figures__item")}>
-                            <span className={cx("figures__item-number")}>
-                                <span>+</span>
-                                <CountUp start={888} end={978} duration={4} />
-                            </span>
-                            <span className={cx("figures__item-des")}>
-                                MEMBERS JOINED
-                            </span>
-                        </div>
-                        <div className={cx("figures__item")}>
-                            <span className={cx("figures__item-number")}>
-                                <span>+</span>
-                                <CountUp start={0} end={50} duration={4} />
-                            </span>
-                            <span className={cx("figures__item-des")}>
-                                FITNESS PROGRAMS
-                            </span>
-                        </div>
-                    </div>
 
-                    {/* Hero btn */}
-                    <div className={cx("hero__buttons")}>
-                        <button className={cx("btn")}>Get Started</button>
-                        <button className={cx("btn")}>Learn More</button>
+                        {/* Hero btn */}
+                        <div className={cx("hero__buttons")}>
+                            <button className={cx("btn")}>Get Started</button>
+                            <button className={cx("btn")}>Learn More</button>
+                        </div>
                     </div>
                 </div>
 
